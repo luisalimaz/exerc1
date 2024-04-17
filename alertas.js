c6fcf462874a87a88631a51beb4910eb66b5471d
Maria Luísa <147561610+luisalimaz@users.noreply.github.com>
2024-04-17 20:13:24 -0300

    Update alertas.js

--- alertas.js
+++ alertas.js
@@ -1,3 +1,3 @@
-document.getElementById("botaoExterno").addEventListener("click", function() {
-    alert("Você clicou no botão!");
-});
+function MostrarMensagem(){
+    alert ('Voce clicou no botão do exemplo 2');
+}
